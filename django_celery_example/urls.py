dac1f35e9355be7a8fde184fbe6ff7647b345cd9
banset <mohitbasnet80@gmail.com>
2024-06-20 14:03:08 +0545

    app started

--- django_celery_example/urls.py
+++ django_celery_example/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_celery project.
+URL configuration for django_celery_example project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
